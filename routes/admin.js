1de0bd5ac04f32d7fc02e23b9f202935b3bf93ef
gabriel-vasconcelos <gabrieltotal6@gmail.com>
2023-01-28 16:32:12 -0300

    criando rota para a pagina de listagem de produtos para o admin

--- routes/admin.js
+++ routes/admin.js
@@ -7,4 +7,10 @@ router.get("/products", function (req, res, next) {
   res.render("products/form", { title: "Products" });
 });
 
+
+/* Route para a página de listagem */
+router.get("/products/list-all", function (req, res, next) {
+  res.render("products/listAll", { title: "Listagem de Produtos" });
+});
+
 module.exports = router;
