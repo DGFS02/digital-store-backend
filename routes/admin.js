ec93a9cee1a12d65a4804d39767905e194329f79
Carlos Marques <carlosxjm@gmail.com>
2023-02-04 16:34:15 -0300

    format all files with prettier

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,9 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
-
 /* Admin panel. */
-router.get("/products", function (req, res, next) {
-  res.render("products/form", { title: "Products" });
+router.get('/products', function (req, res, next) {
+  res.render('products/form', { title: 'Products' });
 });
 
 module.exports = router;
