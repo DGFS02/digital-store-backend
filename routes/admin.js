2480e4aaca26314096c50bc56606636e5c0d06ed
Francisco M <chicaofmp@hotmail.com>
2023-02-04 16:24:59 -0300

    Sending branch to create sidebar and topbar for admin view

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,19 @@
 var express = require("express");
 var router = express.Router();
 
-
 /* Admin panel. */
 router.get("/products", function (req, res, next) {
   res.render("products/form", { title: "Products" });
 });
 
-
 /* Route para a página de listagem */
 router.get("/products/list-all", function (req, res, next) {
   res.render("products/listAll", { title: "Listagem de Produtos" });
+});
+
+//TESTE
+router.get("/", function (req, res, next) {
+  res.render("admin");
 });
 
 module.exports = router;
