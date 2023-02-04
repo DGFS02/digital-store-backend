efbf76d56102041fcfb4b89289889b9965ec4e08
Joao Pedro Lima Ferreira Vieira <108179012+multi2002@users.noreply.github.com>
2023-02-04 15:29:55 -0300

    tela de registro

--- routes/admin.js
+++ routes/admin.js
@@ -7,4 +7,8 @@ router.get("/products", function (req, res, next) {
   res.render("products/form", { title: "Products" });
 });
 
+router.get("/register", function (req, res, next) {
+  res.render('register', { title: "Cadastro de Usuarios" });
+});
+
 module.exports = router;
--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var { Product } = require("../models");
 
+
 router.get("/products", async (req, res, next) => {
   const products = await Product.findAll();
   res.json(products);
@@ -10,8 +11,19 @@ router.get("/products", async (req, res, next) => {
 router.get("/products/:id", async (req, res, next) => {
   const productId = req.params.id;
   const product = await Product.findByPk(productId)
-
   res.json(product);
+});
+
+
+router.post('/register', async (req, res) => {
+  try {
+    const { userName, Email, Password } = req.body;
+    const user = new User({ userName, Email, Password });
+    await user.save();
+    res.status(201).send({ message: 'Usuário criado com sucesso!' });
+  } catch (error) {
+    res.status(400).send({ error: 'Erro ao criar usuário.' });
+  }
 });
 
 module.exports = router;
