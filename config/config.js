a346647860f80211366a63bcc56931b67d758f2d
BrunoAAM <brunoandradeaguiarmarques@hotmail.com>
2023-02-04 10:19:35 -0300

    Adicionando arquivo .env

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config(); 
+module.exports=
+
+{
+  "development": {
+    "username": process.env.DB_UserName,
+    "password": process.env.DB_Password,
+    "database": process.env.DB_Database,
+    "host": process.env.DB_Host,
+    "dialect": process.env.DB_Dialect
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
