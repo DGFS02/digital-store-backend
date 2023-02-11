e9121c7189cd9e99c5bc26cdf828f35592383528
FS02 <rhbpinheiro30@gmail.com>
2023-02-11 15:28:44 -0300

    tela de login

--- routes/admin.js
+++ routes/admin.js
@@ -6,4 +6,8 @@ router.get('/products', function (req, res, next) {
   res.render('products/form', { title: 'Products' });
 });
 
+router.get('/login', function (req, res, next) {
+  res.render('auth/login', { title: 'Login' });
+});
+
 module.exports = router;
