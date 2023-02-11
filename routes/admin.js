8afac840cbce343b23d9e58c19617466780610b7
Gabriel-Vasconcelos <gabrieltotal6@gmail.com>
2023-02-11 15:55:03 -0300

    passando para o form de edit as informações do produto específico

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var { Product } = require('../models');
 
 /* Admin panel. */
 router.get('/products', function (req, res, next) {
   res.render('products/form', { title: 'Products' });
 });
 
-
 /* Rota para a página de listagem de produto */
-router.get("/products/list-all", function (req, res, next) {
-  res.render("products/listAll", { title: "Listagem de Produtos" });
+router.get('/products/list-all', function (req, res, next) {
+  res.render('products/listAll', { title: 'Listagem de Produtos' });
 });
 
 /* Rota para a página de update de produto */
-router.get("/products/edit", function (req, res, next) {
-  res.render("products/edit", { title: "Editar Produtos" });
+router.get('/products/:id/edit', async function (req, res, next) {
+  // Pegando um produto por ID e salvando
+  const product = await Product.findByPk(req.params.id);
+
+  console.log(product);
+  res.render('products/edit', { title: 'Editar Produtos', product });
 });
 
 module.exports = router;
