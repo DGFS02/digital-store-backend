03083f280050e29004f666b8f39863e4573df49b
mrcsporto <mrcsporto@gmail.com>
2023-02-11 16:19:12 -0300

    add encoded token to login

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var { Product } = require('../models');
-var { User } = require('../models');
+var { User, Token } = require('../models');
 
 router.get('/products', async (req, res, next) => {
   const products = await Product.findAll();
@@ -30,19 +30,28 @@ router.post('/products', function (req, res, next) {
 });
 
 router.post('/login', async (req, res, next) => {
-  try {
-    const user = await User.findOne({
-      email: req.body.email,
-      password: req.body.password,
-    });
-    if (user) {
-      res.status(200).json(user);
-    } else {
-      res.status(401).json({ message: 'Email or password is incorrect' });
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+  const user = await User.findOne({
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  if (user) {
+    const jwt = require('jsonwebtoken');
+    const token = jwt.sign({ user }, process.env.JWT_PRIVATE_KEY);
+
+    await Token.build({
+      jwt: token,
+    }).save();
+
+    res.status(201).json({ token });
+  } else {
+    res.status(401).json({ message: 'Email or password is incorrect' });
   }
+  // try {
+  // } catch (error) {
+  //   throw new Error(error)
+  //   res.status(500).json({ message: error.message });
+  // }
 });
 
 router.post('/users', async (req, res, next) => {
